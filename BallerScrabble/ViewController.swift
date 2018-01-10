a82497769d537e7bf259b9eba1971b640c84186f
Zack Kayser <zack.kayser@kroger.com>
2018-01-10 16:03:17 -0500

    Initial Commit

---
+++ BallerScrabble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BallerScrabble
+//
+//  Created by Kayser, Zack (NonEmp) on 1/10/18.
+//  Copyright © 2018 Kayser, Zack (NonEmp). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
